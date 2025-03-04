29eec42e0d3a6ccf90b554d9a8997caa7d42a82f
Aye-Chan009 <ayechan.spmsc@gmail.com>
2025-03-04 22:41:26 +0800

    ( ͡° ͜ʖ ͡°)

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! or not don't care 3=====D</Text>
+      <Text>( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)3=====D</Text>
       <StatusBar style="auto" />
     </View>
   );
